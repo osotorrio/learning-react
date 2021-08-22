e45a560fbf567f01d96c3d249cd2217e7cbf79bf
osotorrio <osotorrio@hotmail.com>
2021-08-22 18:27:59 +0200

    Added flag per item

--- react-app/src/components/Countries/CountryItem.js
+++ react-app/src/components/Countries/CountryItem.js
@@ -1,9 +1,16 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Image from 'react-bootstrap/Image';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Link } from 'react-router-dom';
 
 function CountryItem({ country }) {
+  const imgStyle = {
+    width: '3rem',
+    height: '3rem',
+    margin: '.50rem',
+  };
+
   return (
     <ListGroup.Item>
       <Link
@@ -11,6 +18,7 @@ function CountryItem({ country }) {
           pathname: '/details',
           state: country,
         }}>
+        <Image src={country.flag} style={imgStyle} rounded />
         {country.name}
       </Link>
     </ListGroup.Item>
