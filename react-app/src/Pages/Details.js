c1e0081574fa7769e232f1c655926485ba8b6885
osotorrio <osotorrio@hotmail.com>
2021-08-22 17:50:20 +0200

    Displaying country details

--- react-app/src/Pages/Details.js
+++ react-app/src/Pages/Details.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import { Button, Card, ListGroup } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
 
 function Details() {
   const country = useLocation().state;
   return (
-    <div>
-      <h1>Details</h1>
-      {country.name}
-    </div>
+    <Card style={{ width: '36rem' }}>
+      <Card.Img variant="top" src={country.flag} />
+      <Card.Header>
+        <b>{country.name}</b>
+      </Card.Header>
+      <Card.Body>
+        <ListGroup variant="flush">
+          <ListGroup.Item>
+            <b>Country Codes: </b>
+            {country.alpha2Code}, {country.alpha3Code}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <b>Capital: </b>
+            {country.capital}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <b>Region: </b>
+            {country.region}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <b>Borders: </b>
+            {country.borders.length > 0 ? country.borders.join(', ') : 'None'}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <b>Area: </b>
+            {country.area} km2
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <b>Population: </b>
+            {country.population} people
+          </ListGroup.Item>
+        </ListGroup>
+      </Card.Body>
+      <Button variant="primary">Search another country</Button>
+    </Card>
   );
 }
 
